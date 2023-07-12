ddd78ab0fd27ce690fbc16055fe6454fe6079987
Thomas Szentirmay <tlszent@outlook.com>
2023-07-12 14:16:21 -0400

    added hash to encrypt passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
+const { hash, compare } = require('bcrypt');
 const db = require('../db/connection');
+
 
 class User extends Model { }
 
@@ -20,7 +22,16 @@ User.init({
   }
 }, {
   sequelize: db,
-  modelName: 'user'
+  modelName: 'user',
+  hooks: {
+    beforeCreate(user) {
+      hash(user.password, 10, (err, hash) => {
+        if (err) return console.log(err);
+
+        user.password = hash;
+      });
+    }
+  }
 });
 
 module.exports = User;
