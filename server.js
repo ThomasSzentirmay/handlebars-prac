98928ae49f13a36a18078d381dc5f32fe03ea476
Thomas Szentirmay <tlszent@outlook.com>
2023-07-12 12:54:11 -0400

    fixed ext names

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const { engine } = require('express-handlebars');
 // Import our db connection
 const db = require('./db/connection');
 
+
+
 // Import routes
 const api_routes = require('./controllers/api_routes');
 const view_routes = require('./controllers/view_routes');
@@ -10,21 +12,30 @@ const view_routes = require('./controllers/view_routes');
 const app = express();
 const PORT = process.env.PORT || 3333;
 
+
+
 // Middleware
 app.use(express.json()); // Allows the client/browser to send json in a request
 app.use(express.static('public')); // Allows the client/browser to access any folders or files in public - opens this folder at the root
 
+
+
 // Setup Handlebars Template Engine
-app.engine('handlebars', engine({
+app.engine('hbs', engine({
   // layout directory that allows you to avoid repeated html code
   layoutsDir: './views/layouts',
+  // set extension for your handlebar files to .hbs 
   extname: 'hbs'
 }));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 app.set('views', './views');
+
+
 
 // Load Routes
 app.use('/', [api_routes, view_routes]);
+
+
 
 // Connect to the db and create all tables based off of our models
 db.sync({ force: false })
