9f44c24d55dd378e2d25c191682718e2e51c68f9
Thomas Szentirmay <tlszent@outlook.com>
2023-07-12 13:22:09 -0400

    used inturrpaltion to add active classes to link

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
   res.render('index', {
-
+    isHome: true
   });
 });
 
 router.get('/register', (req, res) => {
-  res.render('register');
+  res.render('register', {
+    isRegister: true
+  });
 })
 
 module.exports = router;
